c178cec80209065c9e0e20af3bbba219aff8df58
Murali... <msmurali2005@gmail.com>
2025-06-26 12:55:09 +0530

    first-commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const multer = require('multer')
+const { exec } = require('child_process')
+const path = require('path')
+const fs = require('fs')
+const { error } = require('console')
+const { stdout, stderr } = require('process')
+const cors = require('cors')
+
+const upload = multer( { dest: 'uploads/' });
+
+const app = express();
+app.use(cors());
+
+app.post('/convert/pdf-to-word', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ message: 'No file uploaded!' });
+    }
+    const pdfPath = req.file.path;
+    const originalName = path.parse(req.file.originalname).name;
+    const outputFileName = originalName + '.docx';
+    const outputPath = path.join(__dirname, 'converted', outputFileName);
+
+    const command = `python pdf-to-docx.py "${pdfPath}" "${outputPath}"`;
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`)
+            return res.status(500).json({ message: 'conversion Failed'})
+        }
+
+        res.download(outputPath, 'converted.docx', (err) => {
+            fs.unlinkSync(pdfPath);
+            fs.unlinkSync(outputPath);
+        })
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on Port 3000')
+})
